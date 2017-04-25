c06443ff3bc65e9fbb1f29cb4404f141fbc5e9d2
nickenilsson <nilsson.sthlm@gmail.com>
2017-04-25 17:07:30 +0200

    Add: ScoreKeeper

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -6,7 +6,11 @@ public class ScoreKeeper : MonoBehaviour {
 
 	// Use this for initialization
 
+<<<<<<< Updated upstream
 	public int currentScore = 0;
+=======
+	public float currentScore = 0;
+>>>>>>> Stashed changes
 	public int currentStreak = 0;
 
 	public int comboBreak1 = 3;
@@ -25,11 +29,20 @@ public class ScoreKeeper : MonoBehaviour {
 	}
 
 
+<<<<<<< Updated upstream
 	public int currentMultiplier(){
 		float comboMultiplier;
 		if (currentStreak >= comboBreak1) {
 			comboMultiplier1 = comboMultiplier1;
 		} else if (currentStreak >= comboBreak2) {
+=======
+	public float currentMultiplier(){
+		float comboMultiplier = 1;
+		if (currentStreak >= comboBreak1) {
+			comboMultiplier = comboMultiplier1;
+		}
+		if (currentStreak >= comboBreak2) {
+>>>>>>> Stashed changes
 			comboMultiplier = comboMultiplier2;
 		}
 
@@ -39,6 +52,7 @@ public class ScoreKeeper : MonoBehaviour {
 	public float answerCorrect(float answerTime = 0){
 
 		float score = maxScorePerQuestion - (answerTime * scorePenaltyPerSecond);
+<<<<<<< Updated upstream
 
 		if(currentScore >= comboBreak1 ) {
 			score = score * comboMultiplier1;
@@ -46,6 +60,12 @@ public class ScoreKeeper : MonoBehaviour {
 		else if (currentScore >= comboBreak2) {
 			score = score * comboMultiplier2;
 		}
+=======
+		score = score * currentMultiplier ();
+
+		Debug.Log ("CurrentStreak" + currentStreak);
+		Debug.Log ("currentMultiplier: " + currentMultiplier());
+>>>>>>> Stashed changes
 
 		currentScore += score;
 		currentStreak += 1;

4c9d886c74329246c8a74e6d4f1b74deb0e5b940
Kristian Nilsen - Mac <sajber@sajber.info>
2017-04-25 11:26:27 +0200

    StufStuff

--- Assets/Scripts/game_logic.cs
+++ Assets/Scripts/game_logic.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class game_logic : MonoBehaviour {
 
+	public Button button1;
+	public Button button2;
+
 	// Use this for initialization
 	void Start () {
-		
+		//button1 = GameObject.Find ("alt_1");
+		//button2 = GameObject.Find ("alt_2");
+		button1.onClick.AddListener(ButtonClicked);
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	public void ButtonClicked(){
+		button1.GetComponentInChildren<Text>().text = "test";
+	}
+
+
+
 }

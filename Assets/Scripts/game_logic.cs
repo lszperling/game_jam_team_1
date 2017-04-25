9165952cacf6a32448f339657e648e3699b799e4
Kristian Nilsen - Mac <sajber@sajber.info>
2017-04-25 11:59:43 +0200

    more things

--- Assets/Scripts/game_logic.cs
+++ Assets/Scripts/game_logic.cs
@@ -7,24 +7,61 @@ public class game_logic : MonoBehaviour {
 
 	public Button button1;
 	public Button button2;
+	public Slider TimerSlider;
+	public Text RightText;
+	public Text WrongText;
 
 	// Use this for initialization
 	void Start () {
 		//button1 = GameObject.Find ("alt_1");
 		//button2 = GameObject.Find ("alt_2");
-		button1.onClick.AddListener(ButtonClicked);
+		button1.onClick.AddListener(() => ButtonClicked(button1));
+		button2.onClick.AddListener(() => ButtonClicked(button2));
+
+
+		//set Question
+		button1.GetComponentInChildren<Text>().text = "Blue";
+		button2.GetComponentInChildren<Text>().text = "Red";
+
+		RightText.GetComponent<Text> ().enabled = false;
+		WrongText.GetComponent<Text> ().enabled = false;
 	
+	}
+
+	void FixedUpdate(){
+		TimerSlider.value -= 0.05f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () {	
 		
 	}
 
-	public void ButtonClicked(){
-		button1.GetComponentInChildren<Text>().text = "test";
+	public void ButtonClicked(Button btn){
+
+
+
+		if (btn.GetComponentInChildren<Text> ().text == "Blue") {
+			TimerSlider.value += 1;
+			RightText.GetComponent<Text> ().enabled = true;
+			WrongText.GetComponent<Text> ().enabled = false;
+
+		} else {
+			RightText.GetComponent<Text> ().enabled = false;
+			WrongText.GetComponent<Text> ().enabled = true;
+		}
+
+		//String answer = getAnswer()
+		//if(button1 == answer){
+
+		//}
+
+
+
+
+		//get new question
+		//button1.GetComponentInChildren<Text>().text = "Answer 1";
+		//button2.GetComponentInChildren<Text>().text = "Answer 2";
+
 	}
-
-
-
 }

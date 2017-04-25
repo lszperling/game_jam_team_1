bc371055e2aa9006d815bba22bd3febf4a06f02d
nickenilsson <nilsson.sthlm@gmail.com>
2017-04-25 13:46:51 +0200

    Add content script

--- Assets/Scripts/content.cs
+++ Assets/Scripts/content.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using System.IO;
+
+
+[System.Serializable]
+class Question {
+	public string correct;
+	public string incorrect;
+	public string imageID;
+}
+[System.Serializable]
+class QuestionList {
+	public List<Question> questions;
+}
+
 
 public class content : MonoBehaviour {
 
-	// Use this for initialization
+	private QuestionList questionList;
 	void Start () {
-		
+		TextAsset asset = Resources.Load (Path.Combine ("Questions", "questions")) as TextAsset;
+		questionList = JsonUtility.FromJson<QuestionList> (asset.text);
+		Debug.Log ("apekatt");
+		Debug.Log (questionList.questions [0].correct);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+	}
+
+	public Question getQuestion() {
+		int randomIndex = Random.Range (0, questionList.questions.Count);
+		return questionList.questions [randomIndex];
 	}
 }

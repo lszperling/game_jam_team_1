750b9bc652927a5f6b95c6c0f929fea21bab9ee4
Kristian Nilsen - Mac <sajber@sajber.info>
2017-04-25 16:33:35 +0200

    added missing files

---
+++ Assets/Scripts/MainMenuLogic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLogic : MonoBehaviour {
+
+
+	public Button StartGameButton;
+
+	// Use this for initialization
+	void Start () {
+
+		StartGameButton.onClick.AddListener(() => StartGame());
+		//button2.onClick.AddListener(() => ButtonClicked(button2));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+
+	private void StartGame(){
+	
+		SceneManager.LoadScene("Game", LoadSceneMode.Single); 
+
+	}
+
+
+
+}

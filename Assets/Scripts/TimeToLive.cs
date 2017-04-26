4c762b1b7c79bc99826bfc354ab375cb60603e02
nickenilsson <nilsson.sthlm@gmail.com>
2017-04-26 14:53:43 +0200

    Add: Animations and prefabs

---
+++ Assets/Scripts/TimeToLive.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeToLive : MonoBehaviour {
+
+	public float lifetime = 1f;
+	private float currentLifeTime = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		currentLifeTime += Time.deltaTime;
+		if (currentLifeTime > lifetime) {
+			Destroy (this.gameObject);
+		}
+	}
+}

3fb8c20b2051a70ad0cd3aaecfec15974e860b94
Kristian Nilsen - Mac <sajber@sajber.info>
2017-04-28 10:56:41 +0200

    Credits menu (for real this time) and unlockable content in mainmenu

--- Assets/Scripts/MainMenuLogic.cs
+++ Assets/Scripts/MainMenuLogic.cs
@@ -3,27 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class MainMenuLogic : MonoBehaviour {
 
 
 	public Button StartGameButton;
+	public Button StartGameButtonTopic2;
+	public Button StartGameButtonTopic3;
+	public Button StartGameButtonTopic4;
+	public Button StartGameButtonTopic5;
+
+	public Text TimeLeftUnlock;
+	public Text TimeLeftUnlock3;
+	public Text TimeLeftUnlock4;
+	public Text TimeLeftUnlock5;
+
+	public GameObject LockImage;
+	public GameObject LockImage2;
+	public GameObject LockImage3;
+	public GameObject LockImage4;
+
 	public Button RulesButton;
+	public Button CreditsButton;
 	public Text HighScore;
-	public Image TransImage;
+	public GameObject TransImageLeft;
+	public GameObject TransImageRight;
 
 	private int highscore = 0;
 	private bool StartGameClicked = false;
 	private bool ShowRulesClicked =  false;
+	private bool CreditsRulesClicked =  false;
 	private int TransWidth;
 	private int TransHeight;
+	private string installDateKey = "InstallDate";
 
 
 	// Use this for initialization
 	void Start () {
 
-		StartGameButton.onClick.AddListener(() => StartGame());
+		if (!PlayerPrefs.HasKey (installDateKey)) {
+			PlayerPrefs.SetString (installDateKey, System.DateTime.Now.ToString());
+		}
+
+		//StartGameButton.onClick.AddListener(() => StartGame());
+
+		StartGameButton.onClick.AddListener(() => StartGame(1));
+
+		if (Topic2Enabled()) {
+			StartGameButtonTopic2.onClick.AddListener(() => StartGame(2));
+		}
+		if (Topic3Enabled()) {
+			StartGameButtonTopic3.onClick.AddListener(() => StartGame(3));
+		}
+		if (Topic4Enabled()) {
+			StartGameButtonTopic4.onClick.AddListener(() => StartGame(4));
+		}
+		if (Topic5Enabled()) {
+			StartGameButtonTopic5.onClick.AddListener(() => StartGame(5));
+		}
+
+
 		RulesButton.onClick.AddListener(() => ShowRules());
+		CreditsButton.onClick.AddListener(() => ShowCredits());
 
 		if (PlayerPrefs.HasKey ("highscore")) {
 			HighScore.GetComponent<Text> ().text = "High score: " + PlayerPrefs.GetInt ("highscore").ToString();
@@ -43,7 +85,7 @@ public class MainMenuLogic : MonoBehaviour {
 
 		if (StartGameClicked) {
 		
-			RectTransform TransRectTrans = TransImage.GetComponent<RectTransform> ();
+			RectTransform TransRectTrans = TransImageLeft.GetComponent<RectTransform> ();
 		
 			if (TransRectTrans.sizeDelta.x >= 3400 && TransRectTrans.sizeDelta.x >= 3400) {	
 				SceneManager.LoadScene ("Game", LoadSceneMode.Single); 			
@@ -55,7 +97,7 @@ public class MainMenuLogic : MonoBehaviour {
 		} 
 		else if (ShowRulesClicked) {
 
-			RectTransform TransRectTrans = TransImage.GetComponent<RectTransform> ();
+			RectTransform TransRectTrans = TransImageLeft.GetComponent<RectTransform> ();
 
 			if (TransRectTrans.sizeDelta.x >= 3400 && TransRectTrans.sizeDelta.x >= 3400) {
 				SceneManager.LoadScene ("RulesMenu", LoadSceneMode.Single); 
@@ -65,16 +107,104 @@ public class MainMenuLogic : MonoBehaviour {
 				TransRectTrans.sizeDelta = new Vector2 (TransWidth, TransHeight);
 			}		
 		}
+		else if (CreditsRulesClicked) {
+			RectTransform TransRectTrans = TransImageRight.GetComponent<RectTransform> ();
+
+			if (TransRectTrans.sizeDelta.x >= 3400 && TransRectTrans.sizeDelta.x >= 3400) {
+				SceneManager.LoadScene ("CreditsMenu", LoadSceneMode.Single); 
+			} else {
+				TransHeight += 100;
+				TransWidth += 100;
+				TransRectTrans.sizeDelta = new Vector2 (TransWidth, TransHeight);
+			}		
+		}
+
+		TimeSpan timeDifference = GetTimeFromFinalDate ();
+
+		if (!Topic2Enabled()) {
+			TimeLeftUnlock.GetComponent<Text> ().text = CountdownString(timeDifference);
+			LockImage.SetActive (true);
+			TimeLeftUnlock.GetComponent<Text> ().fontSize = 30;
+		}
+
+		if (!Topic3Enabled()) {
+			TimeLeftUnlock3.GetComponent<Text> ().text = CountdownString(timeDifference);
+			LockImage2.SetActive (true);
+		}
+
+		if (!Topic4Enabled()) {
+			TimeLeftUnlock4.GetComponent<Text> ().text = CountdownString(timeDifference);
+			LockImage3.SetActive (true);
+		}
+
+		if (!Topic5Enabled()) {
+			TimeLeftUnlock5.GetComponent<Text> ().text = CountdownString(timeDifference);
+			LockImage4.SetActive (true);
+		}
+
+		Debug.Log (GetTimeFromFinalDate ().Days);
+		switch (GetTimeFromFinalDate ().Days) 
+		{
+		case 4:
+			StartGameButtonTopic3.gameObject.SetActive (false);
+			StartGameButtonTopic4.gameObject.SetActive (false);
+			StartGameButtonTopic5.gameObject.SetActive (false);
+			break;
+
+		case 3:
+			StartGameButtonTopic4.gameObject.SetActive (false);
+			StartGameButtonTopic5.gameObject.SetActive (false);
+			break;
+
+		case 2:
+			StartGameButtonTopic5.gameObject.SetActive (false);
+			break;
+
+		}
 	}
 
-	private void StartGame(){
-		if (!ShowRulesClicked) {
+	private void StartGame(int topic){
+		if (!ShowRulesClicked && !CreditsRulesClicked) {
+			PlayerPrefs.SetInt("selectedTopic",topic);
 			StartGameClicked = true;
 		}
 	}
 	private void ShowRules(){
-		if (!StartGameClicked) {
+		if (!StartGameClicked && !CreditsRulesClicked) {
 			ShowRulesClicked = true;
 		}
+	}
+	private void ShowCredits(){
+		if (!StartGameClicked && !ShowRulesClicked) {
+			CreditsRulesClicked = true;
+		}
+	}
+
+	private bool Topic2Enabled(){
+		return GetTimeFromFinalDate ().Days < 4;
+	}
+
+	private bool Topic3Enabled(){
+		return GetTimeFromFinalDate ().Days < 3;
+	}
+
+	private bool Topic4Enabled(){
+		return GetTimeFromFinalDate ().Days < 2;
+	}
+
+	private bool Topic5Enabled(){
+		return GetTimeFromFinalDate ().Days < 1;
+	}
+
+	private String CountdownString(TimeSpan timeDifference){
+		return timeDifference.Hours + ":" + 
+			timeDifference.Minutes + ":" + timeDifference.Seconds;;
+	}
+
+
+
+	private TimeSpan GetTimeFromFinalDate(){
+		DateTime installDate = Convert.ToDateTime (PlayerPrefs.GetString (installDateKey).ToString ());
+		return installDate.AddDays(5) - System.DateTime.Now;
 	}
 }
